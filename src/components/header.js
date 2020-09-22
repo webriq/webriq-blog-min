e8c2949754c26776d8f6ddb2a28fd39d7e797e0c
Dorell James <galangdj@gmail.com>
2020-09-22 09:59:31 +0800

    Change colour

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `lightblue`,
+      background: `#0658ff`,
       marginBottom: `1.45rem`,
     }}
   >
